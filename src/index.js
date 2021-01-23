0addec9ae8a06420c9b9c71593871bf834b37513
Darwin Smith <darwin911@gmail.com>
2021-01-23 12:51:30 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Paddle from './paddle.js';
+let canvas = document.getElementById('gameCanvas');
+
+let context = canvas.getContext('2d');
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+context.clearRect(0, 0, 800, 600);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(context);
